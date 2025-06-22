7d25ea81f1be9bedf8e494c1f61518fa905f0d69
Juandedios27 <juan.tapia2001@alumnos.ubiobio.cl>
2025-06-22 18:02:37 -0400

    Se habilitó la carga de pacientes en producción.

--- frontend/src/app/services/pacientes.service.ts
+++ frontend/src/app/services/pacientes.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Paciente } from '../models/paciente.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PacientesService {
-  private apiUrl = 'http://localhost:8081/api/pacientes';
+  private apiUrl = `${environment.apiBase}/pacientes`;
 
   constructor(private http: HttpClient) { }
 
